2158e153f4e5802a2d35b34210294fcfa376a3f3
mehari <gmehari.edu@gmail.com>
2019-01-03 12:01:26 +0300

    :zap: update router

--- static/vue/src/router.js
+++ static/vue/src/router.js
@@ -8,7 +8,8 @@ import MyProfile from '@/components/MyProfile.vue';
 import Agendas from '@/components/Agendas.vue';
 import AgendaDetail from '@/components/AgendaDetail.vue';
 import NotFoundPage from '@/components/404.vue';
-// import AuthService from './services/auth.service';
+import NewAgenda from '@/components/NewAgenda.vue';
+import { AuthService } from '@/services';
 
 Vue.use(Router);
 
@@ -21,23 +22,19 @@ const router = new Router({
       component: Login
     },
     {
-      path: '/forgot-password',
-      name: 'forgot-password',
-      component: ForgotPassword
-    },
-    {
-      path: '/reset-password/:resetToken',
-      name: 'reset-password',
-      component: PasswordReset
-    },
-    {
       path: '/auxilio',
       component: AuthContent,
+      redirect: '/auxilio/agendas',
       children: [
         {
-          path: '',
+          path: 'agendas',
           name: 'agendas',
           component: Agendas
+        },
+        {
+          path: 'create-agenda',
+          name: 'create-agenda',
+          component: NewAgenda
         },
         {
           path: 'detail/:id',
@@ -52,30 +49,32 @@ const router = new Router({
       ]
     },
     {
-      path: '*',
-      name: '404',
-      redirect: '/404'
+      path: '/forgot-password',
+      name: 'forgot-password',
+      component: ForgotPassword
     },
     {
-      path: '/404',
+      path: '/reset-password/:resetToken',
+      name: 'reset-password',
+      component: PasswordReset
+    },
+    {
+      path: '*',
+      name: 'not-found',
       component: NotFoundPage
     }
   ]
 });
 
-// router.beforeEach((to, from, next) => {
-//   const authenticated = AuthService.isAuthenticated();
-//   if (authenticated && to.name === 'login') {
-//     next({ name: 'agendas' });
-//   } else if (!authenticated && to.name === 'forgot-password') {
-//     next({ name: 'forgot-password' });
-//   } else if (!authenticated && to.name === 'login') {
-//     next();
-//   } else if (!authenticated) {
-//     next({ name: 'login' });
-//   } else {
-//     next();
-//   }
-// });
+router.beforeEach((to, from, next) => {
+  const authenticated = AuthService.isAuthenticated();
+  if (!authenticated && to.name !== 'login') {
+    next({ name: 'login' });
+  } else if (authenticated && to.name === 'login') {
+    next({ name: 'agendas' });
+  } else {
+    next();
+  }
+});
 
 export default router;
